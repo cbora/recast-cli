6254d1d8193361145601790bc421350a1f63006f
Lukas Heinrich <lukas.heinrich@gmail.com>
2015-04-01 11:10:57 -0400

    analysis listing

--- recastcli/cli.py
+++ recastcli/cli.py
@@ -1,9 +1,10 @@
 import click
 import recastapi.request
+import recastapi.analysis
 
 def request_fmt():
   fmt =\
-'''\
+u'''\
 ========================
 RECAST request -- {uuid}
 ------------------------
@@ -13,10 +14,32 @@ Status: {status}\
 '''
   return fmt
 
+def analysis_fmt():
+  fmt =\
+u'''\
+========================
+RECAST analysis -- {title}
+------------------------
+UUID: {uuid}
+Collaboration: {collaboration}
+# of requests: {number_of_requests}\
+'''
+  return fmt
 
 @click.group()
 def cli():
     pass
+
+@cli.command(name = 'list-analyses')
+def list_analyses():
+    for p in recastapi.analysis.analysis():
+      click.echo(analysis_fmt().format(**p))
+
+@cli.command(name = 'list-analysis')
+@click.argument('uuid')
+def list_analysis(uuid):
+  click.echo(analysis_fmt().format(**recastapi.analysis.analysis(uuid)))
+
 
 @cli.command(name = 'list-requests')
 def list_requests():
